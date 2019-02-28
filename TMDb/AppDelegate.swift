37836cd1704e9fac66a1f640a7c29c1b2c41976d
Alan Casas <alancasasarevalo@gmail.com>
2019-02-28 21:35:26 +0100

    [FEATURE]  - Storyboard and default VC were deleted  - First commit

---
+++ TMDb/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  TMDb
+//
+//  Created by Alan Casas on 28/02/2019.
+//  Copyright © 2019 Alan Casas. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = UIColor.cyan
+        
+        window?.rootViewController = UIViewController()
+        
+        return true
+    }
+
+}
+
