ffa4b711fcb77162997e6a83f519a3aefe874f59
Alan Casas <alancasasarevalo@gmail.com>
2019-02-28 21:39:21 +0100

    [FEATURE]
    - App core framework was added

---
+++ APP Core/APP_Core.h
@@ -0,0 +1,19 @@
+//
+//  APP_Core.h
+//  APP Core
+//
+//  Created by Alan Casas on 28/02/2019.
+//  Copyright © 2019 Alan Casas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APP_Core.
+FOUNDATION_EXPORT double APP_CoreVersionNumber;
+
+//! Project version string for APP_Core.
+FOUNDATION_EXPORT const unsigned char APP_CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APP_Core/PublicHeader.h>
+
+
