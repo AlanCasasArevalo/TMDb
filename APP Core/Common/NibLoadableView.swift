287b6568afa650e5bb8655d8c72ebc4781a885fb
Alan Casas <alancasasarevalo@gmail.com>
2019-03-01 21:51:00 +0100

    [FEATURE]
    - Date utilities implemented
    - NibLoadableView implemented to get the current name to pass to our collection or table cells
    - Generic table and collection views

---
+++ APP Core/Common/NibLoadableView.swift
@@ -0,0 +1,25 @@
+//
+//  NibLoadableView.swift
+//  APP Core
+//
+//  Created by Alan Casas on 01/03/2019.
+//  Copyright © 2019 Alan Casas. All rights reserved.
+//
+
+import Foundation
+
+protocol NibLoadableView: class {
+    static var nibName: String { get }
+    static func instantiate() -> Self
+}
+
+extension NibLoadableView where Self: UIView {
+    static var nibName: String {
+        return String(describing: self)
+    }
+    
+    static func instantiate() -> Self {
+        return UINib(nibName: nibName, bundle: Bundle(for: Self.self))
+            .instantiate(withOwner: nil, options: nil)[0] as! Self
+    }
+}
