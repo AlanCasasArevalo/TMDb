5111deef00141db1159ddecd87ee48c97b79f2cb
Alan Casas <alancasasarevalo@gmail.com>
2019-03-03 09:02:18 +0100

    [FEATURE] - DetailViewController was added, with scroll and stack view -

---
+++ APP Core/Details/Presentation/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  APP Core
+//
+//  Created by Alan Casas on 03/03/2019.
+//  Copyright © 2019 Alan Casas. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailScrollView: UIScrollView!
+    @IBOutlet weak var detailStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
