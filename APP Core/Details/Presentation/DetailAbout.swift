c8127973e8bbc984bd30d62d0b69a7329b959e76
Alan Casas <alancasasarevalo@gmail.com>
2019-03-03 12:04:37 +0100

    [FEATURE] - Detail about was implemented

---
+++ APP Core/Details/Presentation/DetailAbout.swift
@@ -0,0 +1,16 @@
+//
+//  DetailAbout.swift
+//  APP Core
+//
+//  Created by Alan Casas on 03/03/2019.
+//  Copyright © 2019 Alan Casas. All rights reserved.
+//
+
+import UIKit
+
+class DetailAbout: UIView {
+
+    @IBOutlet weak var detailAboutDetailsLabel: UILabel!
+    @IBOutlet weak var detailAboutTitleLabel: UILabel!
+
+}
